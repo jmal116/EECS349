7ef63bcd3c1ccbfad4815423a0ef7151ecd7852e
Josh <jmal11six@gmail.com>
2019-10-29 10:31:50 -0400

    finished HW3 part 1

---
+++ math/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int x =5, y = 4;
+	int z, n;
+	z = x + y;
+	printf("%d\n", z);
+	return 0;
+}
